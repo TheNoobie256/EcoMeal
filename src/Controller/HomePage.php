80f927a0d5227b00468fdaa58c919fe50ad9d81f
TheNoobie256 <stefi.radu123@yahoo.com>
2026-07-02 12:38:39 +0300

    feat: created database

--- src/Controller/HomePage.php
+++ src/Controller/HomePage.php
@@ -3,12 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomePage extends AbstractController
 {
     #[Route('/')]
-    public function home()
+    public function home(): Response
     {
         return $this->render('mainPage.html.twig');
     }
