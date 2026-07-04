01d6e3406d8d111d8d21432777fe8b50aab7c00e
TheNoobie256 <stefi.radu123@yahoo.com>
2026-07-04 15:47:00 +0300

    test branch

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Form\CategoryFormType;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CategoryController extends AbstractController
+{
+    #[Route('/category', name: 'app_category')]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('category/index.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+
+    #[Route('/category/{id}', name: 'app_category_view')]
+    public function view(Category $category): Response
+    {
+        return $this->render('category/view.html.twig', [
+            'category' => $category,
+        ]);
+    }
+
+    #[Route('/new/category', name: 'app_category_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($category);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_category');
+        }
+
+        return $this->render('category/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
